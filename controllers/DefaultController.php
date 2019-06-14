b00eccb7ae9c0a77a7bf397026bc95e0abc1141a
Markov Artur <36476213+artmarkov@users.noreply.github.com>
2019-06-14 15:10:23 +0300

    update controller

--- controllers/DefaultController.php
+++ controllers/DefaultController.php
@@ -1,12 +1,14 @@
 <?php
 
 namespace artsoft\carousel\controllers;
+
 use Yii;
+use artsoft\controllers\admin\BaseController;
 
 /**
  * CarouselController implements the CRUD actions for artsoft\models\Carousel model.
  */
-class DefaultController extends \backend\controllers\DefaultController 
+class DefaultController extends BaseController 
 {
     public $modelClass       = 'artsoft\carousel\models\Carousel';
     public $modelSearchClass = 'artsoft\carousel\models\search\CarouselSearch';
