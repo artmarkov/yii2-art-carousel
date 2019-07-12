f7715f6810f8c60b06bdff47dbcfcde48325b9dc
Markov Artur <36476213+artmarkov@users.noreply.github.com>
2019-07-12 14:36:49 +0300

    refactor theme

--- views/default/update.php
+++ views/default/update.php
@@ -5,7 +5,7 @@ use yii\helpers\Html;
 /* @var $this yii\web\View */
 /* @var $model artsoft\carousel\models\Carousel */
 
-$this->title = Yii::t('yii', 'Update') . ': ' . $model->name;
+$this->title = Yii::t('art', 'Update "{item}"', ['item' => $model->name]);
 $this->params['breadcrumbs'][] = ['label' => Yii::t('art/carousel', 'Carousel'), 'url' => ['index']];
 $this->params['breadcrumbs'][] = Yii::t('yii', 'Update');
 ?>
