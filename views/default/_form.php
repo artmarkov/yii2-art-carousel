d7ba800df5c3cbad54a232358760b3ee6e5f9542
Markov Artur <36476213+artmarkov@users.noreply.github.com>
2020-02-11 12:05:23 +0300

    Update _form.php

--- views/default/_form.php
+++ views/default/_form.php
@@ -12,138 +12,90 @@ use kartik\switchinput\SwitchInput;
 ?>
 
 <div class="carousel-form">
-
-    <?php 
+    <?php
     $form = ActiveForm::begin([
-            'id' => 'carousel-form',
-            'validateOnBlur' => false,
-        ])
+        'id' => 'carousel-form',
+        'validateOnBlur' => false,
+    ])
     ?>
+    <div class="panel panel-default">
+        <div class="panel-body">
+            <div class="row">
+                <div class="col-md-8">
 
-    <div class="row">
-        <div class="col-md-8">
-
-            <div class="panel panel-default">
-                <div class="panel-body">
-                    
                     <?= $form->field($model, 'name')->textInput(['maxlength' => true]) ?>
 
-                    <?= $form->field($model, 'slug')->textInput(['maxlength' => true]) ?>                     
+                    <?= $form->field($model, 'slug')->textInput(['maxlength' => true]) ?>
+
+                    <?php if (!$model->isNewRecord) : ?>
+
+                        <div class="panel panel-default">
+                            <div class="panel-body">
+
+                                <?= \artsoft\mediamanager\widgets\MediaManagerWidget::widget(['model' => $model]); ?>
+
+                            </div>
+                        </div>
+
+                    <?php endif; ?>
 
                 </div>
-            </div>
-            
-            <?php if (!$model->isNewRecord) : ?>
-            
-            <div class="panel panel-default">
-                <div class="panel-body">
-                    
-                    <?= \artsoft\mediamanager\widgets\MediaManagerWidget::widget(['model' => $model]); ?>
-                    
-                </div> 
-            </div>
-            
-            <?php endif; ?>
-            
-        </div>
 
-        <div class="col-md-4">            
-                    
-            <div class="panel panel-default">
-                <div class="panel-body">
-                    <div class="record-info">
-                        <div class="form-group clearfix">
-                            <label class="control-label" style="float: left; padding-right: 5px;"><?=  $model->attributeLabels()['id'] ?>: </label>
-                            <span><?=  $model->id ?></span>
-                        </div>
-                        <?php if (!$model->isNewRecord): ?>
+                <div class="col-md-4">
 
-                        <div class="form-group clearfix">
-                            <label class="control-label" style="float: left; padding-right: 5px;">
-                                <?= $model->attributeLabels()['created_at'] ?> :
-                            </label>
-                            <span><?= $model->createdDatetime ?></span>
-                        </div>
 
-                        <div class="form-group clearfix">
-                            <label class="control-label" style="float: left; padding-right: 5px;">
-                                <?= $model->attributeLabels()['updated_at'] ?> :
-                            </label>
-                            <span><?= $model->updatedDatetime ?></span>
-                        </div>
+                    <?= $form->field($model->loadDefaultValues(), 'status')->dropDownList(Carousel::getStatusList()) ?>
 
-                        <div class="form-group clearfix">
-                                <label class="control-label" style="float: left; padding-right: 5px;">
-                                    <?= $model->attributeLabels()['updated_by'] ?> :
-                                </label>
-                                <span><?= $model->updatedBy->username ?></span>
+                    <?= $form->field($model, 'items')->textInput() ?>
+
+                    <?= $form->field($model, 'transition_style')->textInput(['maxlength' => true])->hint('fade, backSlide, goDown, fadeUp') ?>
+
+                    <?= $form->field($model, 'auto_play')->textInput(['maxlength' => true]) ?>
+
+
+                    <div class="row">
+                        <div class="col-md-12 col-lg-4 ">
+                            <?= $form->field($model, 'single_item')->widget(SwitchInput::classname(), [
+                                'pluginOptions' => [
+                                    'size' => 'small',
+                                ],
+                            ]); ?>
                         </div>
-                        
-                        <?php endif; ?>
-                    
-                       
-                                
-                        
-                        <div class="form-group">
-                            <?php  if ($model->isNewRecord): ?>
-                                <?= Html::submitButton(Yii::t('art', 'Create'), ['class' => 'btn btn-primary']) ?>
-                                <?= Html::a(Yii::t('art', 'Cancel'), ['/carousel/default/index'], ['class' => 'btn btn-default']) ?>
-                            <?php  else: ?>
-                                <?= Html::submitButton(Yii::t('art', 'Save'), ['class' => 'btn btn-primary']) ?>
-                                <?= Html::a(Yii::t('art', 'Delete'),
-                                    ['/carousel/default/delete', 'id' => $model->id], [
-                                    'class' => 'btn btn-danger',
-                                    'data' => [
-                                        'confirm' => Yii::t('yii', 'Are you sure you want to delete this item?'),
-                                        'method' => 'post',
-                                    ],
-                                ]) ?>
-                            <?php endif; ?>
+                        <div class="col-md-12 col-lg-4 ">
+                            <?= $form->field($model, 'navigation')->widget(SwitchInput::classname(), [
+                                'pluginOptions' => [
+                                    'size' => 'small',
+                                ],
+                            ]); ?>
+                        </div>
+                        <div class="col-md-12 col-lg-4 ">
+                            <?= $form->field($model, 'pagination')->widget(SwitchInput::classname(), [
+                                'pluginOptions' => [
+                                    'size' => 'small',
+                                ],
+                            ]); ?>
                         </div>
                     </div>
                 </div>
             </div>
-            <div class="panel panel-default">
-                <div class="panel-body">
-                    
-                        <?= $form->field($model->loadDefaultValues(), 'status')->dropDownList(Carousel::getStatusList()) ?>
-
-                        <?= $form->field($model, 'items')->textInput() ?>
-                     
-                        <?= $form->field($model, 'transition_style')->textInput(['maxlength' => true])->hint('fade, backSlide, goDown, fadeUp') ?>
-
-                        <?= $form->field($model, 'auto_play')->textInput(['maxlength' => true]) ?>
-                        
-                        
-                        <div class="row">
-                            <div class="col-md-12 col-lg-4 "> 
-                        <?= $form->field($model, 'single_item')->widget(SwitchInput::classname(), [
-                        'pluginOptions' => [
-                            'size' => 'small',
-                        ],
-                        ]); ?>                                 
-                            </div>
-                            <div class="col-md-12 col-lg-4 "> 
-                        <?= $form->field($model, 'navigation')->widget(SwitchInput::classname(), [
-                        'pluginOptions' => [
-                            'size' => 'small',
-                        ],
-                        ]); ?>                                 
-                            </div>
-                            <div class="col-md-12 col-lg-4 ">
-                        <?= $form->field($model, 'pagination')->widget(SwitchInput::classname(), [
-                        'pluginOptions' => [
-                            'size' => 'small',
-                        ],
-                        ]); ?>                                                              
-                            </div>
-                        </div>
-                </div>
-            </div>
-
         </div>
+        <div class="panel-footer">
+            <div class="form-group">
+                <?= Html::a(Yii::t('art', 'Go to list'), ['/carousel/default/index'], ['class' => 'btn btn-default']) ?>
+                <?= Html::submitButton(Yii::t('art', 'Save'), ['class' => 'btn btn-primary']) ?>
+                <?php if (!$model->isNewRecord): ?>
+                    <?= Html::a(Yii::t('art', 'Delete'),
+                        ['/carousel/default/delete', 'id' => $model->id], [
+                            'class' => 'btn btn-danger',
+                            'data' => [
+                                'confirm' => Yii::t('yii', 'Are you sure you want to delete this item?'),
+                                'method' => 'post',
+                            ],
+                        ]) ?>
+                <?php endif; ?>
+            </div>
+        </div>
+
+        <?php ActiveForm::end(); ?>
+
     </div>
-
-    <?php  ActiveForm::end(); ?>
-
-</div>
